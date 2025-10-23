e9cca480c06018a4ea88f7f8b7b9a35bcc253130
syerlytawiramandhilasyerlytawi-dev <syerlytawiramandhilasyerlytawi@gmail.com>
2025-10-23 10:12:53 +0700

    Correct typos in script.js
    Fix typos in variable and method names in script.js

--- scipt.js
+++ scipt.js
@@ -1,5 +1,5 @@
 // 1. Pilih tombol berdasarkan ID-nya
-const tombol sapa = document.getElementByid('sapaButton')
+const tombol sapa = document.getElementById('sapaButton')
 // 2. Tambahkan ;event listener; untuk aksi 'click'
 tombolSapa.addEventlistener('click',function() {
    // 3. Jalankan kode ini ketika tombol di-klik
