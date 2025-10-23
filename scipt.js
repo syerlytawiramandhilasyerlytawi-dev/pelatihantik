0afed1dfc6277039a651fae991d1ba6aebd54bc5
syerlytawiramandhilasyerlytawi-dev <syerlytawiramandhilasyerlytawi@gmail.com>
2025-10-23 10:11:45 +0700

    Correct button event listener and syntax errors
    Fix syntax errors in script.js for button functionality.

---
+++ scipt.js
@@ -0,0 +1,7 @@
+// 1. Pilih tombol berdasarkan ID-nya
+const tombol sapa = document.getElementByid('sapaButton')
+// 2. Tambahkan ;event listener; untuk aksi 'click'
+tombolSapa.addEventlistener('click',function() {
+   // 3. Jalankan kode ini ketika tombol di-klik
+   alert('Halo! Terima kasih sudah berkunjung!);
+});
